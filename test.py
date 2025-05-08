3bfdff1f789ff984f83706dd84316db8af8241e3
Pritam Kamble <7276279026.pk@gmail.com>
2025-05-08 17:45:47 +0530

    add additional git commands to documentation

--- test.py
+++ test.py
@@ -3,3 +3,8 @@ print('Hello')
 # git init - To creaeate a new git repository
 # git status - To check the status of the repository
 # git add - To stage changes for commit
+# git commit -m "" - To commit the staged changes
+# git log - To view the commit history
+# create new repository on GitHub
+# git remote add origin <repository-url> - To add a remote repository
+# git push -u origin main - To push the changes to the remote repository
